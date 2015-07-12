bb4986ee884d7628f551568c2b518842aa2af8c8
Carlos Llamacho <llamacholancho@gmail.com>
2015-07-11 23:49:06 -0400

    Added second behaviour. After the first player moves the board cannot be considered empty.

--- test_tictac.py
+++ test_tictac.py
@@ -15,3 +15,12 @@ class TestsTicTacToe:
         """
         game = TicTac()
         assert game.board.is_empty()
+
+    def test_after_player_one_moves_board_is_not_empty(self):
+        """
+        After the player one makes a move to the board,
+        then the board of the game cannot be considered empty.
+        """
+        game = TicTac()
+        game.player_one.make_move(1, 1)
+        assert not game.board.is_empty()
--- tictac.py
+++ tictac.py
@@ -11,6 +11,7 @@ class TicTac(object):
 
     def __init__(self):
         self.board = Board()
+        self.player_one = Player(self, 'x')
 
 
 class Board(object):
@@ -18,5 +19,46 @@ class Board(object):
     Game board.
     """
 
+    def __init__(self):
+        row = [0, 0, 0]
+        self._body = [row, row, row]
+
     def is_empty(self):
+        """
+        Verify that the board is empty. A board is empty if all the rows
+        are contain only 0.
+        """
+        for row in self._body:
+            for column in row:
+                if bool(column):
+                    return False
         return True
+
+    def place_move(self, mark, *args):
+        """
+        Place a move into the board with the requested mark.
+
+        :param mark: String, unique mark.
+        :param coord_x: Integer, x position.
+        :param coord_y: Integer, y position.
+        """
+        self._body[args[0]][args[1]] = mark
+
+
+class Player(object):
+    """
+    A tic tac player.
+    """
+
+    def __init__(self, game, mark):
+        self.game = game
+        self.player_mark = mark
+
+    def make_move(self, coord_x, coord_y):
+        """
+        Make a move into a game board using a x,y coordinates point.
+
+        :param coord_x: Integer, must be between 0 and 2.
+        :param coord_y: Integer, must be between 0 and 2.
+        """
+        self.game.board.place_move(self.player_mark, coord_x, coord_y)
